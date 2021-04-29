f816c3464f35498626ba71c4493048ff55983f34
Martin Sjölund <martin@sjoelund.se>
2021-04-29 13:12:26 +0200

    Removed some broken elements
    See .scripts/remove-header.py

---
+++ .scripts/remove-header.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import os
+import fnmatch
+
+for root, dir, files in os.walk("."):
+  if ".git" in root:
+    continue
+  for f in files:
+    if f.endswith(".html"):
+      path = os.path.join(root, f)
+      with open(path) as fin:
+        lines = fin.readlines()
+        found = False
+        for (needle, ndel) in [
+            ('    <li id="siteaction-user_login"><a\n',3),
+            ('    <li id="siteaction-accessibility"><a\n',3),
+            ('    <li id="siteaction-imprint"><a\n',3),
+            ('<div id="cse-search-form" style="width: 100%;">Loading</div>\n',1),
+            ('<div id="portal-colophon">\n',16)
+          ]:
+          if needle in lines:
+            found = True
+            ix = lines.index(needle)
+            del lines[ix:ix+ndel]
+        for (needle, replace) in [('See also <a href="#">Imprint</a>.', '')]:
+          for i in range(1,len(lines)):
+            if needle in lines[i]:
+              found = True
+              lines[i] = lines[i].replace(needle, "")
+            
+      if found:
+        with open(path, "w") as fout:
+          for line in lines:
+            fout.write(line)
+  #print(root)
+  #print("")
+  #for items in fnmatch.filter(files, "*"):
+  #        print("..." + items)
+  #print("")
